82afec3ba4161fe130b512cdb091e51060a2de17
uzz-ptl7 <uzair07patel@gmail.com>
2024-11-01 22:49:01 +0200

    Understood to use API's

--- API/script.js
+++ API/script.js
@@ -3,4 +3,20 @@ fetch('https://jsonplaceholder.typicode.com/posts')
     // .then(data => console.log(data));
     .then(data => {
         const postDiv = document.getElementById('posts');
+        data.forEach(post => {
+            // if (post.userId === 1) {
+                const postElement = document.createElement('div');
+                postElement.classList.add('post');
+                postElement.innerHTML = `
+                    <h2>${post.title}</h2>
+                    <p>${post.body}</p>
+                `;
+                postDiv.appendChild(postElement);
+            // }
+        })
     })
+    .catch(error =>{
+        // console.log(error);
+        console.error('Error:', error);
+        document.getElementById('posts').innerHTML = '<h3 style="color:red;">Sorry Something went wrong!</h3>';
+    });
